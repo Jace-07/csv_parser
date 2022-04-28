f17950bc65bead4d00c94f8bc082344afc15dc6c
Jace-sec <27819222+Jace-07@users.noreply.github.com>
2022-04-28 17:43:08 +0200

    Add files via upload
    Initial commit with buggy code and basic/no sorting algorithms.......
    working prototype and general framework achieved....
    not very much but most important part is the remaining bits....

---
+++ csv_parser/py-solution/parse-sheet2.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+# by jace mitnick
+
+
+import pandas as pd
+
+#
+
+# sales = pd.read_excel('TUK-TUK-04.2022.xlsx')
+
+
+ex_file = "../TUK-TUK-04.2022.xlsx"
+
+
+# create new read_excel/csv pandas dataframe in variable
+names = pd.read_excel(ex_file, usecols=['DSA'])
+
+d_sales = pd.read_excel(ex_file, usecols=['Package', 'Value'])
+
+
+# try converting to json
+#new_json = names.to_json()
+
+"""
+# names of the dsa
+names = []
+# package = []
+amount = []
+
+for a,b in rows:
+    names.append(a.value)
+    # package.append(s.value)
+    amount.append(b.value)
+
+
+for i in names:
+	for j in d_sales:
+		print(f'name:{i} \n')
+		print(f'sale:{j} \n')"""
+
+
+
+# function to sort the names into unique occurrence
+# memo pad used for the sorting
+# def sort_names(dnames, memo={}, sorted_names=[]):
+	# # iterator to crawl the file length
+	# for i in range(len(dnames):
+	# 	# name variable to fetch names from list
+	# 	for name in dnames:
+	# 		if name in memo:
+	# 			sorted_names += memo[name]
+	# 			i = i + 1
+	# 		else:
+	# 			sorted_names = memo[name]
+	# 			i = i + 1
+	# return sorted_names
+	
+
+
+
+# check dataframe content
+print(names, d_sales)
---
+++ csv_parser/r-solution/README.md
@@ -0,0 +1,5 @@
+[! spreadsheet formating tool in R]
+
+[_source: "https://www.marsja.se/r-excel-tutorial-how-to-read-and-write-xlsx-files-in-r/"]
+
+
